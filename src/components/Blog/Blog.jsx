b1677aed6cee363dd2dcd4af6cce0d051de74f48
Md Ishaque <iamishaque.business@gmail.com>
2022-05-26 16:49:06 +0600

    set up blog and myPortfolio page

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,26 +3,27 @@ import React from 'react'
 export default function Blog() {
     const data = [
         {
-            icon: "/images/value.png",
-            title: "Customers",
-            state: "10K+"
+            q: "How will you improve the performance of a React Application?",
+            a: "React by default design such a way that give you faster speed without specifically do anything about performance optimization.However yet if I am  facing any problem with the speed of my app I can follow these steps.First of all,I always use the minified build version for production.Make sure  deploy the right build folder in my production server.Secondly,if my application required to load hundreds,thousands or millions of data i can use 'windowing' technique to reduce the loading time.I use react-virtualized library for this purpose."
         },
         {
-            icon: "/images/revenue.png",
-            title: "Annual Revenue",
-            state: "3M+"
+            q: "What are the different ways to manage a state in a React application?",
+            a: "There are mostly three way we use to manage a state in a React application at functional component.These are useState hook,useReducer hook and Context Api.For manage state in local capacity we use useState hook.it is very handy.for more complex state management we use useReducer instead of useState.However when it requires to manage state in global capacity we can use Context API."
         },
         {
-            icon: "/images/rating.png",
-            title: "Reviews",
-            state: "3K+"
+            q: "How does prototypical inheritance work?",
+            a: "We use prototypical inheritance ,when we need to inherit properties and functions from a different object.We can get a default property in object called '__proto__'.We can use that in a child object to get the access to it parent's properties and functions.example:__proto__:parentObjectName."
         },
         {
-            icon: "/images/product.png",
-            title: "Products",
-            state: "30k+"
+            q: "You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?",
+            a: "Well,first i will get the value from the search input box.Then I will check if it is a valid input string or not.If yes,then I will filter out all the product of this array  which name includes the search result.I will be using filter() and includes() method for this purpose.After that i will create a new array to show the result."
+        },
+        {
+            q: "What is a unit test? Why should write unit tests?",
+            a: "A unit test is a process to test a piece of code that can be isolated logically in a system.The result of the test let you know how your code performing.It is very important part of a development pase.When  users interact with your application,there  create thousands of use cases.If those use cases do not handle properly beforehand,then application will crash.So,as a developer it impossible to remember all the use case and write  the perfect codes that will handle every use case.Thats why many people prefer TDD or Test Driven Development to avoid unnecessary errors and crashes."
         },
     ]
+
 
     return (
         <section>
@@ -31,32 +32,24 @@ export default function Blog() {
                     <span className="absolute inset-x-0 h-px -translate-y-1/2 bg-black/10 top-1/2"></span>
 
                     <h2 className="relative inline-block px-4 text-2xl font-bold text-center bg-white capitalize">
-                        We have served
+                        Our Blog
                     </h2>
                 </div>
-
-                <section className="text-center">
-                    <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-                        <ul className="grid grid-cols-2 gap-4 border-2 border-slate-600 rounded-xl lg:grid-cols-4">
+                <section class="text-gray-600 body-font overflow-hidden">
+                    <div class="container px-5 py-24 mx-auto">
+                        <div class="flex flex-wrap -m-12">
                             {
-                                data.map((item, i) => {
-                                    return (<li className="p-8 flex flex-col justify-center items-center" key={i}>
-                                        <img src={item.icon} alt="" className='w-24 h-24' />
-
-                                        <p className="text-2xl font-extrabold text-pink-500">{item.state}</p>
-                                        <p className="mt-1 text-lg font-medium text-slate-500">{item.title}</p>
-                                    </li>)
-                                })
+                                data?.map((blog, index) => (
+                                    <div class="px-12 md:w-1/2 flex flex-col items-start" key={index}>
+                                        <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">{blog.q}</h2>
+                                        <p class="leading-relaxed mb-8">{blog.a}</p>
+                                    </div>
+                                ))
                             }
 
-
-
-
-                        </ul>
+                        </div>
                     </div>
                 </section>
-
-
             </div>
         </section>
     )
--- src/components/MyPortfolio/MyPortfolio.jsx
+++ src/components/MyPortfolio/MyPortfolio.jsx
@@ -17,21 +17,21 @@ export default function MyPortfolio() {
           <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto flex flex-wrap flex-col">
 
-              <div className="flex flex-col text-center w-full">
-                <h1 className="text-4xl font-medium title-font mb-4 text-gray-900">Md Ishaq</h1>
+              <div className="flex flex-col text-center w-full bg-emerald-400 text-white py-5" style={{boxShadow:'inset 0 8px 8px -8px rgba(0, 0, 0, 0.3), inset 0 -8px 8px -8px rgba(0, 0, 0, 0.3)'}}>
+                <h1 className="text-5xl drop-shadow-2xl  title-font mb-4 font-bold">Md Ishaq</h1>
                 <p className="lg:w-2/3 mx-auto leading-relaxed text-base font-bold">ishaqrabbu97@gmail.com</p>
               </div>
-              <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto flex flex-wrap">
+              <section className="text-gray-600 body-font ">
+                <div className="container px-5 py-24 mx-auto flex  flex-wrap">
 
 
                   <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
                     <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
                       <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                     </div>
-                    <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">1</div>
+                    <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">1</div>
                     <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                      <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                      <div className="flex-shrink-0 w-24 h-24 bg-slate-100 text-slate-500 rounded-full inline-flex items-center justify-center">
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-12 h-12" viewBox="0 0 24 24"><path d="M17 7c-2.094 0-3.611 1.567-5.001 3.346C10.609 8.567 9.093 7 7 7c-2.757 0-5 2.243-5 5a4.98 4.98 0 0 0 1.459 3.534A4.956 4.956 0 0 0 6.99 17h.012c2.089-.005 3.605-1.572 4.996-3.351C13.389 15.431 14.906 17 17 17c2.757 0 5-2.243 5-5s-2.243-5-5-5zM6.998 15l-.008 1v-1c-.799 0-1.55-.312-2.114-.878A3.004 3.004 0 0 1 7 9c1.33 0 2.56 1.438 3.746 2.998C9.558 13.557 8.328 14.997 6.998 15zM17 15c-1.33 0-2.561-1.44-3.749-3.002C14.438 10.438 15.668 9 17 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z"></path></svg>
                       </div>
@@ -70,9 +70,9 @@ export default function MyPortfolio() {
                     <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
                       <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                     </div>
-                    <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">2</div>
+                    <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">2</div>
                     <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                      <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                      <div className="flex-shrink-0 w-24 h-24 bg-slate-100 text-slate-500 rounded-full inline-flex items-center justify-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-12 h-12" viewBox="0 0 24 24"><path d="M4 4h4v4H4zm6 0h4v4h-4zm6 0h4v4h-4zM4 10h4v4H4zm6 0h4v4h-4zm6 0h4v4h-4zM4 16h4v4H4zm6 0h4v4h-4zm6 0h4v4h-4z"></path></svg>
                       </div>
                       <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
@@ -91,9 +91,9 @@ export default function MyPortfolio() {
                     <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
                       <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                     </div>
-                    <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">3</div>
+                    <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">3</div>
                     <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                      <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                      <div className="flex-shrink-0 w-24 h-24 bg-slate-100 text-slate-500 rounded-full inline-flex items-center justify-center">
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-12 h-12" viewBox="0 0 24 24"><path d="M2 7v1l11 4 9-4V7L11 4z"></path><path d="M4 11v4.267c0 1.621 4.001 3.893 9 3.734 4-.126 6.586-1.972 7-3.467.024-.089.037-.178.037-.268V11L13 14l-5-1.667v3.213l-1-.364V12l-3-1z"></path></svg>
                       </div>
